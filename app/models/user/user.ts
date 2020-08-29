b8b10896048a1974c0a4fc850062ea03ba4ba990
zulian-uki <hiruma1997@gmail.com>
2020-08-29 14:31:18 +0700

    create user state and model

---
+++ app/models/user/user.ts
@@ -0,0 +1,61 @@
+import { Instance, SnapshotOut, types, flow } from "mobx-state-tree"
+import { withEnvironment } from "../extensions/with-environment"
+import { GithubLoginResult } from "../../services/api/api.types"
+import { omit } from "ramda"
+import { save, load } from "../../utils/keychain"
+
+export interface UserDetails {
+  username: string
+  url: string
+  avatarUrl: string
+}
+
+/**
+ * Model description here for TypeScript hints.
+ */
+export const UserModel = types
+  .model("User")
+  .props({
+    userDetails: types.frozen<UserDetails>(),
+    password: types.maybe(types.string),
+  })
+  .postProcessSnapshot(omit(["password"])) // prevent password to be stored in async storage, instead use secure keychain
+  .extend(withEnvironment)
+  .views((self) => ({})) // eslint-disable-line @typescript-eslint/no-unused-vars
+  .actions((self) => ({
+    setUser(userDetails: UserDetails, password: string) {
+      self.userDetails = userDetails
+      self.password = password
+    },
+    logout() {
+      self.password = ""
+      self.userDetails = null
+    },
+  }))
+  .actions((self) => ({
+    login: flow(function * (login: string, password: string) {
+      const result: GithubLoginResult = yield self.environment.api.loginToGithub(login, password)
+      console.log(result)
+      if (result.kind === "ok") {
+        self.setUser(result.user, password)
+        yield save(result.user.username, password)
+      }
+    }),
+    loadPassword: flow(function * () {
+      const { password } = yield load()
+      self.password = password
+    }),
+  })) // eslint-disable-line @typescript-eslint/no-unused-vars
+
+/**
+  * Un-comment the following to omit model attributes from your snapshots (and from async storage).
+  * Useful for sensitive data like passwords, or transitive state like whether a modal is open.
+
+  * Note that you'll need to import `omit` from ramda, which is already included in the project!
+  *  .postProcessSnapshot(omit(["password", "socialSecurityNumber", "creditCardNumber"]))
+  */
+
+type UserType = Instance<typeof UserModel>
+export interface User extends UserType {}
+type UserSnapshotType = SnapshotOut<typeof UserModel>
+export interface UserSnapshot extends UserSnapshotType {}
