6dd35c913eea79d103f7ea337ed7f83591b57791
zulian-uki <hiruma1997@gmail.com>
2020-08-29 21:26:06 +0700

    add menu actions component

---
+++ app/components/menu-actions/menu-actions.story.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react-native"
+import { StoryScreen, Story, UseCase } from "../../../storybook/views"
+import { color } from "../../theme"
+import { MenuActions } from "./menu-actions"
+
+storiesOf("MenuActions", module)
+  .addDecorator(fn => <StoryScreen>{fn()}</StoryScreen>)
+  .add("Style Presets", () => (
+    <Story>
+      <UseCase text="Primary" usage="The primary.">
+        <MenuActions style={{ backgroundColor: color.error }} />
+      </UseCase>
+    </Story>
+  ))
---
+++ app/components/menu-actions/menu-actions.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { observer } from "mobx-react-lite"
+import { Icon, TopNavigationAction, OverflowMenu, MenuItem } from "@ui-kitten/components"
+import { useStores } from "../../models/root-store/root-store-context"
+import { translate } from "../../i18n"
+
+const MenuIcon = (props) => <Icon {...props} name="more-vertical" />
+
+const LogoutIcon = (props) => <Icon {...props} name="log-out" />
+
+const MoonIcon = (props) => <Icon {...props} name="moon-outline" />
+const SunIcon = (props) => <Icon {...props} name="sun-outline" />
+
+export const MenuActions = observer(function MenuActions() {
+  const [menuVisible, setMenuVisible] = React.useState(false)
+  const { darkMode, setDarkMode } = useStores()
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible)
+  }
+
+  const renderMenuAction = () => <TopNavigationAction icon={MenuIcon} onPress={toggleMenu} />
+
+  const renderOverflowMenuAction = () => (
+    <React.Fragment>
+      <OverflowMenu anchor={renderMenuAction} visible={menuVisible} onBackdropPress={toggleMenu}>
+        {darkMode ? (
+          <MenuItem
+            onPress={() => setDarkMode(false)}
+            accessoryLeft={SunIcon}
+            title={translate("settings.light_mode")}
+          />
+        ) : (
+          <MenuItem
+            onPress={() => setDarkMode(true)}
+            accessoryLeft={MoonIcon}
+            title={translate("settings.dark_mode")}
+          />
+        )}
+        <MenuItem accessoryLeft={LogoutIcon} title="Logout" />
+      </OverflowMenu>
+    </React.Fragment>
+  )
+
+  return <>{renderOverflowMenuAction()}</>
+})
