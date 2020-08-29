d1cd4cce6c9eb82009fb5a22c0ed1a07480965a7
zulian-uki <hiruma1997@gmail.com>
2020-08-29 18:12:39 +0700

    change input styling

--- app/components/input/input.tsx
+++ app/components/input/input.tsx
@@ -1,13 +1,6 @@
 import * as React from "react"
-import { Input as KittenInput, InputProps, useTheme } from "@ui-kitten/components"
+import { Input as KittenInput, InputProps } from "@ui-kitten/components"
 
 export function Input(props: InputProps) {
-  const theme = useTheme()
-  const TEXT_STYLE = { marginLeft: 0, paddingLeft: 0 }
-  const CONTAINER_STYLE = {
-    borderColor: "transparent",
-    backgroundColor: "transparent",
-    borderBottomColor: theme["color-basic-transparent-300"],
-  }
-  return <KittenInput textStyle={TEXT_STYLE} style={CONTAINER_STYLE} {...props} />
+  return <KittenInput {...props} />
 }
