2e6bbc4460ab5779d6f5018400e86b30377b527b
zulian-uki <hiruma1997@gmail.com>
2020-08-29 20:27:23 +0700

    add username checks to login screen

--- app/screens/login-screen/login-screen.tsx
+++ app/screens/login-screen/login-screen.tsx
@@ -1,47 +1,43 @@
 import React, { useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle, SafeAreaView, Image, ImageStyle } from "react-native"
-import { Text, Screen, Input } from "../../components"
+import { ViewStyle, SafeAreaView, Image, ImageStyle, TextInput, StyleSheet } from "react-native"
+import { Text, Screen, Input, LoadingIndicator } from "../../components"
 import { translate } from "../../i18n"
 import { Layout, Button } from "@ui-kitten/components"
 import { useStores } from "../../models/root-store/root-store-context"
 import { useNavigation } from "@react-navigation/native"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-export const logoWhite = require("../../../assets/image/logo_white.png")
-export const logoBlack = require("../../../assets/image/logo_black.png")
+const logoWhite = require("../../../assets/image/logo_white.png")
+const logoBlack = require("../../../assets/image/logo_black.png")
 
-const ROOT: ViewStyle = {
-  flex: 1,
-  paddingHorizontal: 24,
-}
-
-const HEADER: ViewStyle = {
-  backgroundColor: "#fff",
-  flexGrow: 2,
-}
-
-const HEADER_IMAGE_CONTAINER: ViewStyle = {
-  flexGrow: 2,
-  justifyContent: "flex-end",
-}
-
-const HEADER_IMAGE: ImageStyle = {
-  width: 50,
-  height: 50,
-}
-const HEADER_TEXT_CONTAINER: ViewStyle = {
-  justifyContent: "center",
-  flexGrow: 3,
-}
-
-const CONTENT: ViewStyle = {
-  flexGrow: 3,
-}
-
-const FOOTER: ViewStyle = {
-  height: 50,
-}
+const styles = StyleSheet.create({
+  content: {
+    flexGrow: 3,
+  },
+  footer: {
+    height: 50,
+  },
+  header: {
+    flexGrow: 2,
+  },
+  headerImage: {
+    height: 50,
+    width: 50,
+  },
+  headerImageContainer: {
+    flexGrow: 2,
+    justifyContent: "flex-end",
+  },
+  headerTextContainer: {
+    flexGrow: 3,
+    justifyContent: "center",
+  },
+  root: {
+    flex: 1,
+    paddingHorizontal: 24,
+  },
+})
 
 export const LoginScreen = observer(function LoginScreen() {
   // Pull in one of our MST stores
@@ -49,44 +45,59 @@ export const LoginScreen = observer(function LoginScreen() {
   const { darkMode, user } = useStores()
   const [status, setStatus] = useState("basic")
   const [caption, setCaption] = useState("")
-  const { setUsername } = user
+  const [loading, setLoading] = useState(false)
+  const { checkUsername } = user
   // OR
   // const rootStore = useStores()
   const [usernameForm, setUsernameForm] = useState("")
-  const onPressNext = () => {
+  const onPressNext = async () => {
+    setCaption("")
+    setStatus("basic")
     if (usernameForm.length === 0) {
       setStatus("danger")
       setCaption("login.username_validation_1")
     } else {
-      setStatus("basic")
-      setUsername(usernameForm)
-      setCaption("")
+      setLoading(true)
+      try {
+        const user = await checkUsername(usernameForm)
+        if (user !== undefined) {
+          navigation.navigate("password")
+        } else {
+          setStatus("danger")
+          setCaption("login.username_validation_2")
+        }
+      } finally {
+        setLoading(false)
+      }
     }
   }
   return (
-    <Screen style={ROOT}>
+    <Screen style={styles.root}>
       <SafeAreaView />
-      <Layout style={HEADER}>
-        <Layout style={HEADER_IMAGE_CONTAINER}>
-          <Image source={darkMode ? logoWhite : logoBlack} style={HEADER_IMAGE} />
+      <Layout style={styles.header}>
+        <Layout style={styles.headerImageContainer}>
+          <Image source={darkMode ? logoWhite : logoBlack} style={styles.headerImage} />
         </Layout>
-        <Layout style={HEADER_TEXT_CONTAINER}>
+        <Layout style={styles.headerImageContainer}>
           <Text category="h1" tx="login.greetings" />
           <Text category="p1" tx="login.greetings_description" />
         </Layout>
       </Layout>
-      <Layout style={CONTENT}>
+      <Layout style={styles.content}>
         <Input
           label="Username"
           status={status}
           caption={caption !== "" ? translate(caption) : ""}
           value={usernameForm}
+          autoCapitalize="none"
           onChangeText={(nextValue) => setUsernameForm(nextValue)}
         />
       </Layout>
       <SafeAreaView>
-        <Layout style={FOOTER}>
-          <Button onPress={onPressNext}>{translate("common.next")}</Button>
+        <Layout style={styles.footer}>
+          <Button onPress={onPressNext}>
+            {loading ? <LoadingIndicator status="basic" /> : translate("common.next")}
+          </Button>
         </Layout>
       </SafeAreaView>
     </Screen>
