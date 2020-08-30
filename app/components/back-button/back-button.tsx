00a6d7d1f1c162c6b58e5027da14405911ac8556
zulian-uki <hiruma1997@gmail.com>
2020-08-30 09:25:07 +0700

    add mobx observer

--- app/components/back-button/back-button.tsx
+++ app/components/back-button/back-button.tsx
@@ -3,6 +3,8 @@ import { Icon } from "@ui-kitten/components"
 import { TouchableOpacity } from "react-native-gesture-handler"
 import { useNavigation } from "@react-navigation/native"
 import { StyleSheet } from "react-native"
+import { useStores } from "../../models"
+import { observer } from "mobx-react-lite"
 
 const styles = StyleSheet.create({
   icon: {
@@ -11,11 +13,12 @@ const styles = StyleSheet.create({
   },
 })
 
-export function BackButton() {
+export const BackButton = observer(function BackButton() {
   const navigation = useNavigation()
+  const { darkMode } = useStores()
   return (
     <TouchableOpacity onPress={navigation.goBack}>
-      <Icon fill="#000" style={styles.icon} name="arrow-back-outline" />
+      <Icon fill={!darkMode ? "#000" : "#fff"} style={styles.icon} name="arrow-back-outline" />
     </TouchableOpacity>
   )
-}
+})
